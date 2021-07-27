f11ca7dfb1a8cbce2526b77ed9d09041727afde7
Henrique Muller <hmullermail@gmail.com>
2021-07-27 02:57:21 -0300

    v0.1 : Simple Multiple Files Project

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # flask_from_scratch, by Henrique Müller, 2021
 
 V0.0 : Getting Started
+v0.1 : Simple Multiple Files Project
--- project.py
+++ project.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Flask from Scratch - v0.0 : Getting Started"
-
-
+from app import app
